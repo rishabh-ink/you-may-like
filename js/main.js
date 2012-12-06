607f96422a5ed588d50d7b5ceb511a1fee5b427f
Rishabh Rao <rishabhsrao@yahoo.com>
2012-12-06 19:43:26 +0530

    Added ajax.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,65 @@
-
 jQuery(document).ready(function() {
-   
+   var ItemModel = function() {
+      var self = this;
+
+      self.name = ko.observable("No name");
+      self.type = ko.observable("No type");
+
+      return {
+         name: self.name,
+         type: self.type
+      };
+   };
+
+   var ItemViewModel = function() {
+      var self = this;
+
+      self.items = ko.observableArray();
+
+      self.initialize = function() {
+         var harryPotter = new ItemModel();
+         harryPotter.name("Harry Potter");
+         harryPotter.type("Book");
+
+         var castle = new ItemModel();
+         castle.name("Richard Castle");
+         castle.type("TV Show");
+
+         self.items.push(harryPotter);
+         self.items.push(castle);
+
+         self.getItems("harry potter");
+      };
+
+      self.getItems = function(query) {
+         jQuery.ajax({
+           url: "http://www.tastekid.com/ask/ws",
+           type: "GET",
+           dataType: "jsonp",
+           async: true,
+           cache: true,
+           jsonp: "jsonp",
+           data: {
+               q: query,
+               format: "JSON"
+           },
+           beforeSend: function(jqXHR, settings) {
+               console.log(jqXHR, settings);
+           },
+           complete: function(jqXHR, settings) {
+               console.log(jqXHR, settings);
+           },
+           error: function(jqXHR, textStatus, errorThrown) {
+               console.log(jqXHR, textStatus, errorThrown);
+           },
+           success: function(data, textStatus, jqXHR) {
+               console.log(data, textStatus, jqXHR);
+           }
+         });
+      };
+
+      self.initialize();
+   };
+
+   ko.applyBindings(new ItemViewModel());
 });
